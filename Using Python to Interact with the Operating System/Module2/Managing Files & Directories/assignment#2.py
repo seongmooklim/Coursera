9974ed8a295112c9293bd815ceba97ce32ff4ffd
seongmooklim <159662590+seongmooklim@users.noreply.github.com>
2024-02-24 23:27:35 +0900

    Update assignment#2.py

--- Using Python to Interact with the Operating System/Module2/Managing Files & Directories/assignment#2.py
+++ Using Python to Interact with the Operating System/Module2/Managing Files & Directories/assignment#2.py
@@ -7,7 +7,7 @@ import os
 
 def new_directory(directory, filename):
   # Before creating a new directory, check to see if it already exists
-  if os.path.isdir(directory) == False:
+  if not os.path.isdir(directory):
     os.mkdir(directory)
 
   # Create the new file inside of the new directory
