2143219faef5845e2c1fbe0341e8051c59def956
seongmooklim <159662590+seongmooklim@users.noreply.github.com>
2024-02-25 23:43:24 +0900

    Update generate_report.py

--- Using Python to Interact with the Operating System/Module2/Managing Files & Directories/generate_report.py
+++ Using Python to Interact with the Operating System/Module2/Managing Files & Directories/generate_report.py
@@ -9,5 +9,25 @@ def read_employees(csv_file_location):
         employee_list.append(data)
     return employee_list
 
+def process_data(employee_list):
+    department_list=[]
+    for employee_data in employee_list:
+        department_list.append(employee_data['Department'])
+        department_data = {}
+        for department_name in set(department_list):
+            department_data[department_name] = department_list.count(department_name)
+    return department_data
+
+def write_report(dictionary, report_file):
+    with open(report_file, "w+") as f:
+        for k in sorted(dictionary):
+            f.write(str(k)+':'+str(dictionary[k])+'\n')
+        f.close()
+
 employee_list = read_employees('../data/employees.csv')
-print(employee_list)
+#print(employee_list)
+
+dictionary = process_data(employee_list)
+#print(dictionary)
+
+write_report(dictionary, '../test_report.txt')
