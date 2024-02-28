e77fa546200910e1347e176baf1ab4795379d77a
seongmooklim <159662590+seongmooklim@users.noreply.github.com>
2024-02-28 17:17:18 +0900

    Update generate_report.py

--- Using Python to Interact with the Operating System/Module2/Managing Files & Directories/generate_report.py
+++ Using Python to Interact with the Operating System/Module2/Managing Files & Directories/generate_report.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 import csv
+
 def read_employees(csv_file_location):
     csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
-    employee_file = csv.DictReader(open(csv_file_location), dialect='empDialect')
+    employee_file = csv.DictReader(open(csv_file_location), dialect = 'empDialect')
+
     employee_list = []
-    for data in employee_file:
-        employee_list.append(data)
+    for data in employee_file: employee_list.append(data)
+
     return employee_list
 
+employee_list = read_employees('/home/student-00-60133f5d8157/data/employees.csv')
+#print(employee_list)
+
 def process_data(employee_list):
-    department_list=[]
+    department_list = []
     for employee_data in employee_list:
         department_list.append(employee_data['Department'])
         department_data = {}
@@ -18,16 +23,14 @@ def process_data(employee_list):
             department_data[department_name] = department_list.count(department_name)
     return department_data
 
+dictionary = process_data(employee_list)
+print(dictionary)
+
 def write_report(dictionary, report_file):
     with open(report_file, "w+") as f:
         for k in sorted(dictionary):
             f.write(str(k)+':'+str(dictionary[k])+'\n')
         f.close()
 
-employee_list = read_employees('../data/employees.csv')
-#print(employee_list)
 
-dictionary = process_data(employee_list)
-#print(dictionary)
-
-write_report(dictionary, '../test_report.txt')
+write_report(dictionary, '/home/student-00-60133f5d8157/test_report.txt')
